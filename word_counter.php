f54ebfdcbc85019d0f56c2c49357c12aef6a1782
Sofany <871377+sofany@users.noreply.github.com>
2021-09-09 17:28:50 +0200

    basic: variable

--- word_counter.php
+++ word_counter.php
@@ -1,12 +1,14 @@
 <?php
 declare(strict_types=1);
 
-function countWord(): int {
-    return 0;
+function countWord(string $phrase): int {
+    $counter = 0;
+
+    return $counter;
 }
 
-function display(): void {
-    echo 'Phrase "" has ' . countWord() . ' word(s).';
+function display(string $phrase): void {
+    echo "Phrase \"$phrase\" has ".countWord($phrase)." word(s).";
 }
 
-display();
+display('Hello world !');
