df82af64c897d443102734c667f257d2ceb392e2
Sofany <871377+sofany@users.noreply.github.com>
2021-09-09 17:14:00 +0200

    basic: function

---
+++ word_counter.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+function countWord(): int {
+    return 0;
+}
+
+function display(): void {
+    echo 'Phrase "" has ' . countWord() . ' word(s).';
+}
+
+display();
